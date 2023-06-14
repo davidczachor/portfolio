5253c1ab405d167d37aca0163a652df56d79fc5a
davidczachor <david@czachor.dev>
2023-06-14 09:50:33 -0400

    Header Component

---
+++ components/Header.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { SocialIcon } from "react-social-icons";
+import { motion } from "framer-motion";
+
+type Props = {};
+
+export default function Header({}: Props) {
+  return (
+    <header className="sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center">
+      <motion.div
+        initial={{
+          opacity: 0,
+          x: -500,
+          scale: 0.5,
+        }}
+        animate={{
+          opacity: 1,
+          x: 0,
+          scale: 1,
+        }}
+        transition={{
+          duration: 1,
+        }}
+        className="flex flex-row items-center"
+      >
+        <SocialIcon
+          url="https://linkedin.com/in/davidczachor"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+        <SocialIcon
+          url="https://github.com/davidczachor"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+      </motion.div>
+      <motion.div
+        initial={{
+          opacity: 0,
+          x: 500,
+          scale: 0.5,
+        }}
+        animate={{
+          opacity: 1,
+          x: 0,
+          scale: 1,
+        }}
+        transition={{
+          duration: 1,
+        }}
+        className="flex flex-row items-center text-gray-300 cursor-pointer"
+      >
+        <SocialIcon network="email" fgColor="gray" bgColor="transparent" />
+        <p className="uppercase hidden md:inline-flex text-sm text-gray-400">
+          {" "}
+          Get in touch
+        </p>
+      </motion.div>
+    </header>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Header from "@/components/Header";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -5,7 +6,7 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <div>
-      {/* Header */}
+      <Header />
       {/* Hero */}
       {/* About */}
       {/* Experiences */}
