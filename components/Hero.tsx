778129cb103bc00a0814de2d07cd36da55c2bb19
davidczachor <david@czachor.dev>
2023-06-14 11:15:19 -0400

    Hero section

---
+++ components/Hero.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { Cursor, useTypewriter } from "react-simple-typewriter";
+import BackgroundCircles from "./BackgroundCircles";
+import Image from "next/image";
+import ProfilePicture from "../public/images/profile.jpg";
+import Link from "next/link";
+
+type Props = {};
+
+export default function Hero({}: Props) {
+  const [text, count] = useTypewriter({
+    words: ["Hi, My Name's David Czachor", "I'm a Software Developer"],
+    loop: true,
+    delaySpeed: 2000,
+  });
+  return (
+    <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
+      <BackgroundCircles />
+      <Image
+        className="relative rounded-full h-32 w-32 mx-auto object-cover"
+        src={ProfilePicture}
+        width={100}
+        height={100}
+        alt="Profile Picture"
+      />
+      <div className="z-20">
+        <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">
+          Software Developer
+        </h2>
+        <h1 className="text-5xl lg:text-6xl font-semibold px-10">
+          <span className="mr-3">{text}</span>
+          <Cursor cursorColor="#F7AB0A" />
+        </h1>
+        <div className="pt-5">
+          <Link href="#about">
+            <button className="heroButton">About</button>
+          </Link>
+          {/* <Link href="#experience">
+            <button className="heroButton">Experience</button>
+          </Link> */}
+          <Link href="#projects">
+            <button className="heroButton">Projects</button>
+          </Link>
+          <Link href="#blog">
+            <button className="heroButton">Blog</button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,17 @@
 import Header from "@/components/Header";
+import Hero from "@/components/Hero";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <div>
+    <div className="h-screen snap-y snap-mandatory overflow-scroll z-0">
       <Header />
-      {/* Hero */}
+
+      <section id="hero" className="snap-center">
+        <Hero />
+      </section>
       {/* About */}
       {/* Experiences */}
       {/* Skills */}
